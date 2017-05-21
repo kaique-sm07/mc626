d907f597ed84f0f4216d25abf858c77e06e099dd
Kaique de Souza Monteiro <kaiquesm07.ksm@gmail.com>
2017-05-21 16:24:13 -0300

    Basket

--- bodgeit-master/src/com/thebodgeitstore/pageObjects/BasketPage.java
+++ bodgeit-master/src/com/thebodgeitstore/pageObjects/BasketPage.java
@@ -1,5 +1,44 @@
 package com.thebodgeitstore.pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class BasketPage {
+	
+	private static WebElement element = null;
+	
+	public static WebElement link_basic(WebDriver driver){
+		 
+        element = driver.findElement(By.linkText("Basic Widget"));
+
+        return element;
+
+     }
+	
+	public static WebElement button_plus(WebDriver driver){
+		 
+        element = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[2]/form/center/table/tbody/tr[2]/td[4]/a[2]/img"));
+
+        return element;
+
+     }
+	public static WebElement button_minus(WebDriver driver){
+		 
+        element = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[3]/td/table/tbody/tr/td[2]/form/center/table/tbody/tr[2]/td[4]/a[1]/img"));
+
+        return element;
+
+     }
+	public static WebElement button_submit(WebDriver driver){
+		 
+        element = driver.findElement(By.id("submit"));
+
+        return element;
+
+     }
+	
+	
+	
 
 }
