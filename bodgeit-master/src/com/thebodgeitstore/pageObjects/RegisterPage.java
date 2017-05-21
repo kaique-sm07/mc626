5350735ad5fe684c1719ea9972c667f3f401c08e
julianny-favinha <julianny.favinha@gmail.com>
2017-05-21 16:05:48 -0300

    Métodos static para encontrar os elementos da página de registrar user

--- bodgeit-master/src/com/thebodgeitstore/pageObjects/RegisterPage.java
+++ bodgeit-master/src/com/thebodgeitstore/pageObjects/RegisterPage.java
@@ -1,5 +1,29 @@
 package com.thebodgeitstore.pageObjects;
 
-public class RegisterPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class RegisterPage {
+	private static WebElement element = null;
+	
+	public static WebElement lnk_Username(WebDriver driver) {
+		element = driver.findElement(By.name("username"));
+		return element;
+	}
+	
+	public static WebElement lnk_Password1(WebDriver driver) {
+		element = driver.findElement(By.name("password1"));
+		return element;
+	}
+	
+	public static WebElement lnk_Password2(WebDriver driver) {
+		element = driver.findElement(By.name("password2"));
+		return element;
+	}
+	
+	public static WebElement lnk_Submit(WebDriver driver) {
+		element = driver.findElement(By.id("submit"));
+		return element;
+	}
 }
