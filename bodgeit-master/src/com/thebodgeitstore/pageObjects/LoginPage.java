0df242b6666953119d10bfa503726b2c9523c70d
José Pinto <jose.pinto@evoluservices.com>
2017-05-22 12:37:25 -0300

    Criação de testes da tela de Login.
    Não consegui compilar por problemas do intelliJ. Faço isso de noite.

--- bodgeit-master/src/com/thebodgeitstore/pageObjects/LoginPage.java
+++ bodgeit-master/src/com/thebodgeitstore/pageObjects/LoginPage.java
@@ -1,5 +1,25 @@
 package com.thebodgeitstore.pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
 
+  private static WebElement element = null;
+
+  public static WebElement lnk_Username(WebDriver driver) {
+    element = driver.findElement(By.name("username"));
+    return element;
+  }
+
+  public static WebElement lnk_Password(WebDriver driver) {
+    element = driver.findElement(By.name("password"));
+    return element;
+  }
+
+  public static WebElement lnk_Submit(WebDriver driver) {
+    element = driver.findElement(By.id("submit"));
+    return element;
+  }
 }
